a1f291d7f5399b93f4f2e8226059d1714508960d
Samuel Kellar <sam.kellar@outlook.com>
2016-04-03 06:31:07 -0400

    Added Toolbar to MainActivity and SchedulerActivity, and 'HELP' action menu

--- app/src/main/java/com/medschedulr/medschedulr/SchedulerActivity.java
+++ app/src/main/java/com/medschedulr/medschedulr/SchedulerActivity.java
@@ -1,18 +1,46 @@
 package com.medschedulr.medschedulr;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class SchedulerActivity extends Activity {
+public class SchedulerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduler);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.id_toolbar_scheduler);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.string_title_scheduler);
     }
 
     public void onAddMed(View view) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_scheduler, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_scheduler_help:
+                Toast.makeText(this, "SCHEDULER SOMEONE HELP ME!", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

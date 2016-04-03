4db65bd8d3b4f272ff706cad0f7988bf0e506a4c
Akai-Karasu <Alexanderhsieh95@gmail.com>
2016-04-03 05:34:50 -0400

    Added DetailsDialogFragment Added DatePickerFragment Added TimePickerFragment

---
+++ app/src/main/java/com/medschedulr/medschedulr/DetailsDialogFragment.java
@@ -0,0 +1,27 @@
+package com.medschedulr.medschedulr;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+public class DetailsDialogFragment extends Activity {
+
+    private static final int loaderID = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+}
